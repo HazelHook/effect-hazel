e932f1d43a365a1703396a5f669ec69125e856d1
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-10-14 00:42:02 +0200

    add errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,4 +1,20 @@
 import { Data } from "effect"
 
-export class FetchError extends Data.TaggedError("FetchError") {}
-export class JsonError extends Data.TaggedError("JsonError") {}
+export class ProviderNotFoundError extends Data.TaggedError("ProviderNotFoundError")<{
+	message: string
+	provider: string
+}> {
+	constructor(provider: string) {
+		super({ message: `Provider "${provider}" not found.`, provider })
+	}
+}
+
+export class CollectionNotFoundError extends Data.TaggedError("CollectionNotFoundError")<{
+	message: string
+	collection: string
+	provider: string
+}> {
+	constructor(provider: string, collection: string) {
+		super({ message: `Collection "${collection}" not found for provider ${provider}.`, collection, provider })
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { HttpService } from "./services/http-service"
 
 import * as PgDrizzle from "@effect/sql-drizzle/Pg"
 import { PgClient } from "@effect/sql-pg"
+import { CollectionNotFoundError, ProviderNotFoundError } from "./errors"
 import { Providers } from "./services/providers/providers-service"
 
 const PgLive = PgClient.layer({
@@ -19,9 +20,19 @@ const MainRuntime = ManagedRuntime.make(Providers.Default)
 const program = Effect.gen(function* () {
 	const providers = yield* Providers
 
-	const stripeProvider = yield* Effect.map(TMap.get(providers, "stripe"), Option.getOrThrow)
+	const stripeProvider = yield* Effect.map(
+		TMap.get(providers, "stripe"),
+		Option.getOrThrowWith(() => new ProviderNotFoundError("stripe")),
+	)
+	const clerkProvider = yield* Effect.map(
+		TMap.get(providers, "clerk"),
+		Option.getOrThrowWith(() => new ProviderNotFoundError("clerk")),
+	)
 
-	const stripeCustomerApi = yield* Effect.map(TMap.get(stripeProvider, "customers"), Option.getOrThrow)
+	const stripeCustomerApi = yield* Effect.map(
+		TMap.get(stripeProvider, "customers"),
+		Option.getOrThrowWith(() => new CollectionNotFoundError("clerk", "customers")),
+	)
 
 	const res = yield* stripeCustomerApi.getEntry("customers", yield* Config.string("TEST_TOKEN"), "cus_PNOSY2QB1DXged")
 	const res2 = yield* stripeCustomerApi.getEntries("customers", yield* Config.string("TEST_TOKEN"), {
