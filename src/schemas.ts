0e4ed0fe021238daad6cd3dfeeae64b434e79213
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-10-14 00:43:05 +0200

    remove stuff

--- src/schemas.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from "@effect/schema"
-
-export class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
-	id: Schema.Number,
-	order: Schema.Number,
-	name: Schema.String,
-	height: Schema.Number,
-	weight: Schema.Number,
-}) {}
