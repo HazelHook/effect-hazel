4d852a49cb7b12eedb5f611c906dd63364095023
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-11-21 23:26:46 +0100

    bump wrangler

--- src/routes/collections/api.ts
+++ src/routes/collections/api.ts
@@ -12,10 +12,21 @@ export class CollectionApi extends HttpApiGroup.make("Collection")
 	.add(
 		HttpApiEndpoint.del("deleteCollection", "/collections/:id")
 			.annotate(OpenApi.Summary, "DeleteCollection")
+			.setPath(
+				Schema.Struct({
+					id: Schema.String,
+				}),
+			)
 			.addSuccess(Schema.String),
 	)
 	.add(
-		HttpApiEndpoint.post("updateCollection", "/collections")
+		HttpApiEndpoint.post("updateCollection", "/collections/:id")
 			.annotate(OpenApi.Summary, "UpdateCollection")
+			.setPath(
+				Schema.Struct({
+					id: Schema.String,
+				}),
+			)
+			// .setPayload(Schema.partial(InsertCollection as any))
 			.addSuccess(Collection as any),
 	) {}
