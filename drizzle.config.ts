6353dcdc5e562cc0c16e673064556dce29a00ff8
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-11-23 00:11:18 +0100

    bump deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
 	out: "./src/drizzle",
-	schema: "./src/db/schema.ts",
+	schema: "./src/drizzle/schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
 		url: process.env.POSTGRES_URL!,
